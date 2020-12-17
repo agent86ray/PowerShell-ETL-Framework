e8c3307afaa226ad1244c454c213443abb9a268e
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-17 07:13:48 -0500

    WIP - Initial commit.

---
+++ ImportFramework/ImportFramework.psm1
@@ -0,0 +1,84 @@
+﻿
+# Version information
+$version = @{
+    Version = "1.0.12";
+    RootPath = "C:\IMPORT";
+    LogFolder = "C:\IMPORT"
+    LogFile = "ImportFramework.Log"
+    LogFilePath = Join-Path "C:\IMPORT" "ImportFramework.Log"
+}
+
+
+# SQL Server parameters
+$sqlparameters = @{
+    ServerInstance = "DESKTOP-CH7B7GJ\SQL2019DEV";
+    DatabaseName = "staging";
+}
+
+
+function Get-ImportFrameworkConfiguration {
+
+    $version
+    $sqlparameters
+}
+
+
+function Get-ImportApplication {
+    
+    Get-ChildItem -Path $version["RootPath"] -Attributes "Directory"
+}
+
+
+function Initialize-ImportApplication {
+    Param (
+        [Parameter()]
+            [string] $ApplicationName
+    )
+
+    $ApplicationFolder = Join-Path $version["RootPath"] $ApplicationName
+
+    If (!(Test-Path $ApplicationFolder)) {
+        New-Item -Path $version["RootPath"] -Name $ApplicationName -ItemType "Directory" | Out-Null
+        Log-ImportFramework -Message "Application $ApplicationName created"
+    }
+}
+
+
+function Import-CsvToStaging {
+    Param (
+        [Parameter()]
+            [string] $ApplicationName
+    )
+
+    $ApplicationFolder = Join-Path $version["RootPath"] $ApplicationName
+    $csvfilelist = Get-ChildItem -Path $ApplicationFolder 
+    $csvfilelist 
+
+    #| Select-Object -ExpandProperty Name
+    
+
+    #$Message = "ApplicationName = $ApplicationName"
+    #Log-ImportFramework $Message
+}
+
+
+function Log-ImportFramework {
+    Param (
+        [Parameter()]
+            [string] $Message
+    )
+
+    $RootPath = $version["RootPath"]
+    $LogFile = $version["LogFile"]
+
+    If (!(Test-Path $version["LogFilePath"])) {
+        New-Item -Path $version["LogFolder"] -Name $version["LogFile"] -ItemType "file" | Out-Null
+    }
+
+    $RunTime = Get-Date
+    "$RunTime $Message" | Out-File -FilePath $version["LogFilePath"] -Append
+}
+
+
+
+
---
+++ Test-Importframework.ps1
@@ -0,0 +1,46 @@
+﻿# 
+Get-Module
+
+
+# Remove after making changes to the module
+Remove-Module ImportFramework
+
+
+Get-Help Import-Module -Online
+
+# import module from development environment
+Import-Module -Name "C:\repos\PowerShell-ETL-Framework\ImportFramework"
+
+
+Get-ImportFrameworkVersion
+
+
+
+
+# import any csv files found; will create table if it doesn't exist
+# application name is schema name
+$csvfiles = Import-CsvToStaging -ApplicationName "CRM"
+$csvfiles | Get-Member
+
+
+Get-ImportFrameworkVersion
+
+
+Get-ImportFrameworkConfiguration
+
+
+Initialize-ImportApplication -ApplicationName "CRM"
+
+
+# remove the log file to test creating it
+
+
+
+
+
+
+
+$modulepaths = @()
+$modulepaths = $env:PSModulePath -Split ";"
+$HOMEMODULEFOLDER = $modulepaths[0]
+
