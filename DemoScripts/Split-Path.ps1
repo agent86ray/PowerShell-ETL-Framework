ecd50a7bbd5119304dcc5347471090783ea5c473
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-17 07:58:19 -0500

    New demo script for Split-Path.

---
+++ DemoScripts/Split-Path.ps1
@@ -0,0 +1,15 @@
+﻿# man is an alias for Get-Help
+man Split-Path -online
+
+
+# return folder name portion of path
+$folder = Split-Path -Path "C:\IMPORT\CRM\customer_20201217.csv"
+$folder
+
+
+# return filename portion of path
+$filename = Split-Path -Path "C:\IMPORT\CRM\customer_20201217.csv" -Leaf
+$filename
+
+
+
