3a86d6e512978073ee35233105917b9c7c7c2db7
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-18 06:55:51 -0500

    WIP.

--- DemoScripts/csv-demo.ps1
+++ DemoScripts/csv-demo.ps1
@@ -15,60 +15,6 @@ Get-Service | Export-Csv -Path C:\temp\service.csv -NoTypeInformation
 
 
 # "read" the csv file
-Import-Csv -Path C:\temp\service.csv
-
-
-# load to a SQL Server table using SqlServer module
-# https://docs.microsoft.com/en-us/powershell/module/sqlserver/?view=sqlserver-ps
-
-Import-Module "sqlserver"
-
-Get-SqlInstance -ServerInstance "DESKTOP-CH7B7GJ\SQL2019DEV"
-
-# if fails check the status of the SQL Server Browser service
-Get-Service | Where-Object { $_.DisplayName -like "*sql*" }
-
-
-Start-Service "SQLBrowser"
-
-
-# if fails check if its start type and status
-Get-Service "SQLBrowser" | Select-Object "StartType", "Status"
-
-Get-Service "SQLBrowser" | Select-Object "StartType", "Status" | Get-Member
-
-# if start type = disabled then set it to automatic or manual
-Set-Service -Name "SQLBrowser" -StartupType Manual
-
-# start the service
-Start-Service "SQLBrowser"
-
-
-# create a hash table of the parameters required
-$sqlparameters = @{
-    ServerInstance = "DESKTOP-CH7B7GJ\SQL2019DEV";
-    DatabaseName = "staging";
-    SchemaName = "import";
-    TableName = "services"
-}
-
-# view the hash table
-$sqlparameters
-
-# Insert rows from CSV into table. -Force will create anything that
-# doesn't exist; e.g. database, schema, table
-# Get parameters from $sqlparameters hash table; specify the hash
-# table as a parameter by prefixing variable name with "@"
-Import-Csv -Path C:\temp\service.csv | Write-SqlTableData @sqlparameters -Force
-
-# When -Force creates a table, every column type is NVARCHAR(MAX)
-# Let it create a table with just 1 row then manually change the
-# create table script to varchar(255)
-Import-Csv -Path C:\temp\service.csv | 
-Select-Object -First 1 |
-Write-SqlTableData @sqlparameters -Force
-
-# script create table using SSMS and change column type to varchar(255)
-# execute create table script
+Import-Csv -Path C:\temp\service.csv | Format-Table -AutoSize
 
 
---
+++ DemoScripts/excel-demo.ps1
@@ -0,0 +1,36 @@
+﻿# ImportExcel module available on PowerShell Gallery
+# https://www.powershellgallery.com/packages?q=importexcel
+# The PowerShellGet module provides the lookup on the PowerShell Gallery
+#
+# YouTube videos: https://www.youtube.com/watch?v=U3Ne_yX4tYo&list=PL5uoqS92stXioZw-u-ze_NtvSo0k0K0kq
+#
+# Excel is NOT required to use this module to manipulate an Excel document.
+#
+Find-Module "ImportExcel"
+
+
+# Install directly from the PowerShell Gallery
+Install-Module -Name ImportExcel
+
+
+# Details on ImportExcel module.  
+# Returns TYPE: System.Management.Automation.PSModuleInfo 
+Get-Module -Name ImportExcel | Get-Member
+
+
+# Import if not in the session
+Import-Module -Name ImportExcel
+
+
+# quick and dirty pipe output to excel
+# launches excel as temp file name
+Get-Service | Export-Excel -Now
+
+
+# export to excel file name
+Get-Service | Export-Excel -Path C:\temp\services.xlsx -AutoSize -AutoFilter
+
+
+# Read Excel and Export to CSV
+Import-Excel -Path C:\temp\services.xlsx | Export-Csv -Path C:\temp\servicesfromexcel.csv
+
