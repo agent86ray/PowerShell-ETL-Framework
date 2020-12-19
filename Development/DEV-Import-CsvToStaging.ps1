aa58d098096d8edd5f178d079446f4b1c4445899
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-19 07:36:52 -0500

    Initial commit.
    Got the list of CSV files to import.

---
+++ Development/DEV-Import-CsvToStaging.ps1
@@ -0,0 +1,34 @@
+﻿# develop Import-CsvToStaging
+
+
+# Version information
+$version = @{
+    Version = "1.1.0";
+    RootPath = "C:\IMPORT";
+    LogFolder = "C:\IMPORT";
+    LogFile = "ImportFramework.Log";
+    LogFilePath = Join-Path "C:\IMPORT" "ImportFramework.Log";
+}
+
+$version
+
+# parameters
+[string] $ApplicationName = "CRM"
+
+# Get the ApplicationFolder to import CSV files
+$ApplicationFolder = Join-Path $version["RootPath"] $ApplicationName
+
+# Get list of CSV file names to import
+$filelist = Get-ChildItem -Path $ApplicationFolder "*.csv" | 
+    Select-Object -Property Name
+
+# Get the count of CSV files in the folder
+[int] $count = $filelist | Measure-Object | Select-Object -ExpandProperty Count
+
+if ($count -eq 0) {
+    "TO DO: set exit code and return from cmdlet"
+}
+
+
+$filelist | ForEach-Object { Write-Output $_.Name } 
+
