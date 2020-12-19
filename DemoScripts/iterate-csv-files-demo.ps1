dae99b502925bd0dcfba6ab3a497ace66a15280f
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-19 13:31:11 -0500

    Iterate thru CSV files in folder structure.

---
+++ DemoScripts/iterate-csv-files-demo.ps1
@@ -0,0 +1,12 @@
+﻿# iterate thru CSV files in a folder structure
+
+ForEach(
+    $csvfilepath in get-childitem -path C:\IMPORT -recurse *.csv | 
+    select-object -expand FullName) {
+
+        $filename = Split-Path -Path $csvfilepath -Leaf
+        $tablename = $filename.Split(".")[0]
+
+    "Import CSVFilePath=$csvfilepath Table Name=$tablename"
+}
+
