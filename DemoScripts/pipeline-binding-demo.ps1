5602312855b2181861a5d22aff97e07035f73cab
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-19 09:12:03 -0500

    WIP

---
+++ DemoScripts/pipeline-binding-demo.ps1
@@ -0,0 +1,28 @@
+﻿# pipeline
+
+
+Get-Service | Select-Object -Property Name
+
+
+# Show -Name parameter; Accept pipeline input=true (ByValue, ByPropertyName)
+get-help Get-Service -Full
+
+
+get-content -Path C:\IMPORT\TEXT\SERVICES.txt | Get-Member
+
+
+get-content -Path C:\IMPORT\TEXT\SERVICES.txt | Get-Service
+
+
+# TYPE" System.String
+get-content -Path C:\IMPORT\TEXT\LIST.txt | Get-Member
+
+
+
+
+
+
+get-help Import-Csv -Online
+
+
+
