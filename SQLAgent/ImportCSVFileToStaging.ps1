018130bf554d192047b04d20ca2536de72ece9a5
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-20 12:44:37 -0500

    Sample PowerShell script file to execute from SQL Agent.
    Uses the CmdExec job step type.

---
+++ SQLAgent/ImportCSVFileToStaging.ps1
@@ -0,0 +1,21 @@
+﻿
+$Folder = "C:\repos\PowerShell-ETL-Framework\SQLAgent"
+$LogFile = Join-Path $Folder "Log.txt"
+
+
+# reports Microsoft.PowerShell.Utility as the only module loaded 
+Get-Module | Out-File -FilePath $LogFile -Append
+
+
+Import-Module "sqlserver"
+Import-Module "ImportFramework"
+
+
+$CustomerParameters = @{
+    RootFolder = "C:\EXTERNAL-FILES\CUSTOMER-1";
+}
+
+# import all CSV files available into staging tables
+Import-CsvCustomerFile @CustomerParameters
+
+
