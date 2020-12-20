7e681d4c49d875eaa2a87031c200c1dac17bc1c8
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-20 12:41:29 -0500

    Copy module to prod folder.

---
+++ DemoScripts/deploy-importframework-to-prod.ps1
@@ -0,0 +1,18 @@
+﻿# deploy ImportFramework module for prod
+
+
+# show the module path
+$env:PSModulePath
+
+
+# copy module from dev to prod
+$dev = "C:\repos\PowerShell-ETL-Framework\ImportFramework\ImportFramework.psm1"
+$prod = "C:\Program Files\WindowsPowerShell\Modules\ImportFramework"
+
+if (!(test-path $prod)) {
+    New-Item $prod -ItemType "Directory"
+}
+
+Copy-Item -Path $dev -Destination $prod
+
+
