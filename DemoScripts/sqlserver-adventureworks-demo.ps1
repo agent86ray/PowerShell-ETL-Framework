fe28c90d29b00059e16f2f7ba791c54a8f663a4b
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-12-20 07:54:58 -0500

    Add T-SQL script to restore adventure works database.

---
+++ DemoScripts/sqlserver-adventureworks-demo.ps1
@@ -0,0 +1,24 @@
+﻿# sqlserver demo
+
+$sqlparameters = @{
+    ServerInstance = "DESKTOP-CH7B7GJ\SQL2019DEV";
+    DatabaseName = "AdventureWorksLT2012";
+    SchemaName="SalesLT";
+}
+
+# SELECT * FROM SalesLT.Customer
+Read-SqlTableData -TableName "Customer" @sqlparameters
+
+
+$viewparameters = @{
+    ServerInstance = "DESKTOP-CH7B7GJ\SQL2019DEV";
+    DatabaseName = "AdventureWorksLT2012";
+    SchemaName="Export";
+}
+
+
+# create customer csv file from view
+$exportcsvfilename = "C:\EXTERNAL-FILES\CUSTOMER-1\IMPORT\customer.csv"
+Read-SqlViewData -ViewName "CustomerLookup" -TopN 50 @viewparameters |
+    Export-Csv -Path $exportcsvfilename -NoTypeInformation
+
